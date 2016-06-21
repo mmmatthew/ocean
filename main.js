9f133294f1fc2d94b8045315b90236079b45d981
mmmatthew <matthew.mdv@gmail.com>
2016-06-21 18:02:00 +0200

    implemented simple water level control

--- main.js
+++ main.js
@@ -59,29 +59,7 @@ map = (function () {
 
     // GUI options for rendering modes/effects
     var controls = {
-        'water_level': 200,
-        'RESET ALL' : function() {
-            window.location.reload();
-        },
-        'Directional' : function() {
-            setLight("directional");
-        },
-        'direction_x' : 0.1,
-        'direction_y' : .9,
-        'direction_z' : .51,
-        'direction_toggle' : false,
-        'direction_diffuse' : '#00ffff',
-        'Point' : function() {
-            setLight("point");
-        },
-        'point_x' : 0,
-        'point_y' : 0,
-        'point_z' : 150,
-        'point_toggle' : false,
-        'point_diffuse' : '#ff0000',
-
-        'scale' : 1,
-        'DEMO' : false
+        'water_level': 0
     };
     var directional_mouse = false;
     var point_mouse = false;
@@ -117,54 +95,14 @@ map = (function () {
         gui.domElement.parentNode.style.zIndex = 500;
         window.gui = gui;
 
-        // LIGHTS
-        gui.add(controls, 'RESET ALL');
-        gui.add(controls, 'Directional').name("Directional Light:");
-        gui.add(controls, 'direction_x', -1.0, 1.0).name("&nbsp;&nbsp;direction x").onChange(function(value) {
-            scene.lights.light1.direction[0] = value;
-        });
-        gui.add(controls, 'direction_y', -1.0, 1.0).name("&nbsp;&nbsp;direction y").onChange(function(value) {
-            scene.lights.light1.direction[1] = value;
-        });
-        gui.add(controls, 'direction_z', -1.0, 0.0).name("&nbsp;&nbsp;direction z").onChange(function(value) {
-            scene.lights.light1.direction[2] = value;
-        });
-        gui.add(controls, 'direction_toggle').name("&nbsp;&nbsp;mouse control").onChange(function(value) {
-            directional_mouse = value;
-        });
-        gui.addColor(controls, 'direction_diffuse').name("&nbsp;&nbsp;diffuse").onChange(function(value) {
-            scene.lights.light1.diffuse = hexToRgb(value);
-        });
-        gui.add(controls, 'Point').name("Point Light:");
-        gui.add(controls, 'point_x', -1000, 1000).name("&nbsp;&nbsp;point x").onChange(function(value) {
-            scene.lights.light2.position[0] = value+"px";
-        });
-        gui.add(controls, 'point_y', -1000, 1000).name("&nbsp;&nbsp;point y").onChange(function(value) {
-            scene.lights.light2.position[1] = value+"px";
-        });
-        gui.add(controls, 'point_z', 0, 500).name("&nbsp;&nbsp;point z").onChange(function(value) {
-            scene.lights.light2.position[2] = value+"px";
-        });
-        gui.add(controls, 'point_toggle').name("&nbsp;&nbsp;mouse control").onChange(function(value) {
-            point_mouse = value;
-        });
-        gui.addColor(controls, 'point_diffuse', 0, 2).name("&nbsp;&nbsp;diffuse").onChange(function(value) {
-            scene.lights.light2.diffuse = hexToRgb(value);
-        });
-        gui.add(controls, 'scale', 0, 10).name("terrain scale").onChange(function(value) {
-            scene.styles.terrain.shaders.uniforms.u_scale = value;
-        });
-        gui.add(controls, 'water_level', -100.0, 500.0).name("sea level").onChange(function(value) {
+       
+        gui.add(controls, 'water_level', -5.0, 20.0).name("sea level").onChange(function(value) {
             scene.styles.water.shaders.uniforms.water_level = value;
         });
-        gui.add(controls, 'DEMO').name("DEMO MODE").onChange(function(value) {
-            demo_mode = value;
-        });
+
 
         // reset these to defeat dat.gui's auto-precision setting
-        controls.direction_x = 0;
-        controls.direction_y = 1.;
-        controls.direction_z = -.5;
+        controls.water_level = .1;
         // Iterate over all controllers
         for (var i in gui.__controllers) {
             gui.__controllers[i].updateDisplay();
@@ -177,36 +115,6 @@ map = (function () {
     }
 
 
-    // mouse position listener
-
-    var mouse_monitor = function(e) {
-        var height = document.body.clientHeight;
-        var width = document.body.clientWidth;
-
-        var x = e.clientX;
-        var y = e.clientY;
-        var xpos = ((x - (width / 2)));
-        var ypos = ((y - (height / 2)))*-1.;
-        var xpercent = x/width * 2. - 1.;
-        var ypercent = y/height * 2. - 1.;
-
-        if (directional_mouse) {
-            scene.lights.light1.direction = [xpercent,ypercent*-1,scene.lights.light1.direction[2]];
-            gui.__controllers[2].setValue(scene.lights.light1.direction[0]);
-            gui.__controllers[3].setValue(scene.lights.light1.direction[1]);
-        }
-        if (point_mouse) {
-            // debugger;
-            scene.lights.light2.position = [xpos,ypos,scene.lights.light2.position[2]];
-            gui.__controllers[8].setValue(scene.lights.light2.position[0]);
-            gui.__controllers[9].setValue(scene.lights.light2.position[1]);
-        }
-
-    }
-
-    window.onload = function() {
-      this.addEventListener('mousemove', mouse_monitor);
-    }
 
     /***** Render loop *****/
     window.addEventListener('load', function () {
@@ -220,43 +128,6 @@ map = (function () {
     function preUpdate(will_render) {
         if (!will_render) {
             return;
-        }
-
-        switch(demo_mode) {
-            case true:
-                daycycle();
-                break;
-        }
-    }
-
-    function daycycle() {
-        var d = new Date();
-        var t = d.getTime()/1000;
-
-        var x = Math.sin(t);
-        var y = Math.sin(t+(3.14159/2)); // 1/4 offset
-        var z = Math.sin(t+(3.14159)); // 1/2 offset
-        
-        // offset blue and red
-        var B = x + Math.abs(Math.sin(t+(3.14159*.5)))/4;
-        var G = y * .5 + .5;
-        var R = y * .5 + .5 + Math.abs(Math.sin(t*2))/4;
-        R = Math.max(Math.min(R, 1.), 0);
-        G = Math.max(Math.min(G, 1.), 0);
-        B = Math.max(Math.min(B, 1.), 0);
-
-        var color = [R, G, B, 1];
-        var direction = [x, y, -.5];
-        scene.lights.light1.diffuse = color;
-        // console.log([x, y], color, '=>', rgbToHex(color))
-        controls.direction_diffuse = rgbToHex(color);
-        controls.direction_x = x;
-        controls.direction_y = y;
-        scene.lights.light1.direction = [x, y, -.5];
-
-        // Iterate over all controllers
-        for (var i in gui.__controllers) {
-            gui.__controllers[i].updateDisplay();
         }
     }
 
